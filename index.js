7b52da206551aa78798ff20560ad47407a62331b
Daniel Carl Jones <danny@danielcarl.info>
2017-08-18 11:52:01 +0100

    Move inquirer choices into own const

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-var inquirer = require('inquirer');
+const
+    inquirer = require('inquirer');
+
+const
+    fakeAuthorLs = [
+        {
+            email: 'jane.doe@example.com',
+            name: 'Jane Doe'
+        },
+        {
+            alias: 'Work',
+            email: 'jdoe@business.com',
+            name: 'Jane Doe'
+        }
+    ],
+    choices = fakeAuthorLs.map(author => {
+        return {
+            name: author.alias || `${author.name} <${author.email}>`,
+            value: {
+                email: author.email,
+                name: author.name
+            }
+        }
+    }).concat(
+        [
+            new inquirer.Separator(),
+            'Add new author...',
+            'Remove author...'
+        ]
+    );
+
 
 inquirer.prompt([
     {
         name: 'q1',
         message: 'Which author details would you like to use with this repository?',
         type: 'list',
-        choices: [
-            'Jane Doe <jane.doe@example.com>',
-            'Jane Doe <jdoe@business.com>',
-            new inquirer.Separator(),
-            'Add new author...',
-            'Remove author...'
-        ]
+        choices
     }
 ]).then(console.log).catch(console.error);

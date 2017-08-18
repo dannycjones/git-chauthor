46cbc3aba7a8c2c289000ffad7fe142b851f3bcb
Daniel Carl Jones <danny@danielcarl.info>
2017-08-18 12:30:21 +0100

    Update to load authors from file

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
 const
+    fs = require('fs'),
     inquirer = require('inquirer');
 
 const
-    fakeAuthorLs = [
-        {
-            email: 'jane.doe@example.com',
-            name: 'Jane Doe'
-        },
-        {
-            alias: 'Work',
-            email: 'jdoe@business.com',
-            name: 'Jane Doe'
-        }
-    ],
-    choices = fakeAuthorLs.map(author => {
-        return {
-            name: author.alias || `${author.name} <${author.email}>`,
-            value: {
-                email: author.email,
-                name: author.name
-            }
-        }
-    }).concat(
-        [
-            new inquirer.Separator(),
-            'Add new author...',
-            'Remove author...'
-        ]
-    );
+    configFilePath = process.env.HOME + '/.gitauthors.json',
+    authors = loadConfig(),
+    choices = createChoices(authors);
 
+if (authors) {
+    inquirer.prompt([
+        {
+            name: 'q1',
+            message: 'Which author details would you like to use with this repository?',
+            type: 'list',
+            choices
+        }
+    ]).then(console.log).catch(console.error);
+} else {
+    console.log('No authors defined.');
+}
 
-inquirer.prompt([
-    {
-        name: 'q1',
-        message: 'Which author details would you like to use with this repository?',
-        type: 'list',
-        choices
+function loadConfig() {
+    if (fs.existsSync(configFilePath)) {
+        return JSON.parse(fs.readFileSync(configFilePath));
+    } else {
+        console.log('No config file found');
     }
-]).then(console.log).catch(console.error);
+}
+
+function createChoices(authors) {
+    let choices;
+
+    if (authors) {
+        choices = authors.map(author => {
+            if (typeof author.email !== 'string') { throw new Error('Expected type string for email') }
+            if (typeof author.name !== 'string') { throw new Error('Expected type string for name') }
+
+            return {
+                name: author.alias || `${author.name} <${author.email}>`,
+                value: {
+                    email: author.email,
+                    name: author.name
+                }
+            };
+        }).concat(
+            [
+                new inquirer.Separator(),
+                'Add new author...',
+                'Remove author...'
+            ]
+        );
+    }
+
+    return choices;
+}

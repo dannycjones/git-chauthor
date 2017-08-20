091a5d76adf2e2ebbfa17566fa542fd7f6eba0db
Daniel Carl Jones <danny@danielcarl.info>
2017-08-20 21:28:35 +0100

    Move main body of chauthor.js into main()

--- bin/chauthor.js
+++ bin/chauthor.js
@@ -10,88 +10,90 @@ const
     authors = config.load(),
     authorChoices = createChoices(authors);
 
-if (authors) {
-    inquirer.prompt([
-        {
-            name: 'author',
-            message: 'Which author details would you like to use with this repository?',
-            type: 'list',
-            choices: authorChoices.concat([
-                new inquirer.Separator(),
-                'Manage authors...'
-            ])
-        },
-        {
-            name: 'action',
-            message: 'What would you like to do?',
-            type: 'list',
-            choices: [
-                'Add author',
-                'Remove authors'
-            ],
-            when: answers => answers.author === 'Manage authors...'
-        },
-        {
-            name: 'name',
-            message: 'Author name:',
-            when: answers => answers.action === 'Add author'
-        },
-        {
-            name: 'email',
-            message: 'Author email:',
-            when: answers => answers.action === 'Add author'
-        },
-        {
-            name: 'alias',
-            message: 'Author alias:',
-            when: answers => answers.action === 'Add author'
-        }
-    ]).then(answers => {
-        if (answers.action) {
-            switch (answers.action) {
-                case 'Add author':
-                    authors.push({
-                        alias: answers.alias,
-                        email: answers.email,
-                        name: answers.name
-                    });
-                    config.save(authors);
-                    break;
-                case 'Remove authors':
-                    inquirer.prompt([{
-                        name: 'authors',
-                        message: 'Which authors should be removed?',
-                        type: 'checkbox',
-                        choices: authorChoices
-                    }]).then(answers => {
-                        _.pullAll(authors, answers.authors);
-                        config.save(authors);
-                    }).catch(console.error);
-                    break;
-                default:
-                    console.log('TODO: Take action when user wants to manage authors...');
-                    break;
+function main() {
+    if (authors) {
+        inquirer.prompt([
+            {
+                name: 'author',
+                message: 'Which author details would you like to use with this repository?',
+                type: 'list',
+                choices: authorChoices.concat([
+                    new inquirer.Separator(),
+                    'Manage authors...'
+                ])
+            },
+            {
+                name: 'action',
+                message: 'What would you like to do?',
+                type: 'list',
+                choices: [
+                    'Add author',
+                    'Remove authors'
+                ],
+                when: answers => answers.author === 'Manage authors...'
+            },
+            {
+                name: 'name',
+                message: 'Author name:',
+                when: answers => answers.action === 'Add author'
+            },
+            {
+                name: 'email',
+                message: 'Author email:',
+                when: answers => answers.action === 'Add author'
+            },
+            {
+                name: 'alias',
+                message: 'Author alias:',
+                when: answers => answers.action === 'Add author'
             }
-        } else {
-            git.addConfig('user.name', answers.author.name);
-            git.addConfig('user.email', answers.author.email);
-            console.log('Git author config set.');
-        }
-    }).catch(console.error);
-} else {
-    console.log('No authors found!');
-    inquirer.prompt([
-        {
-            name: 'createConfig',
-            message: 'Would you like to create a new authors config file?',
-            type: 'confirm',
-            default: true
-        }
-    ]).then(answers => {
-        if (answers.createConfig) {
-            config.save([]);
-        }
-    }).catch(console.error);
+        ]).then(answers => {
+            if (answers.action) {
+                switch (answers.action) {
+                    case 'Add author':
+                        authors.push({
+                            alias: answers.alias,
+                            email: answers.email,
+                            name: answers.name
+                        });
+                        config.save(authors);
+                        break;
+                    case 'Remove authors':
+                        inquirer.prompt([{
+                            name: 'authors',
+                            message: 'Which authors should be removed?',
+                            type: 'checkbox',
+                            choices: authorChoices
+                        }]).then(answers => {
+                            _.pullAll(authors, answers.authors);
+                            config.save(authors);
+                        }).catch(console.error);
+                        break;
+                    default:
+                        console.log('TODO: Take action when user wants to manage authors...');
+                        break;
+                }
+            } else {
+                git.addConfig('user.name', answers.author.name);
+                git.addConfig('user.email', answers.author.email);
+                console.log('Git author config set.');
+            }
+        }).catch(console.error);
+    } else {
+        console.log('No authors found!');
+        inquirer.prompt([
+            {
+                name: 'createConfig',
+                message: 'Would you like to create a new authors config file?',
+                type: 'confirm',
+                default: true
+            }
+        ]).then(answers => {
+            if (answers.createConfig) {
+                config.save([]);
+            }
+        }).catch(console.error);
+    }
 }
 
 function createChoices(authors) {
@@ -111,3 +113,5 @@ function createChoices(authors) {
 
     return choices;
 }
+
+main();

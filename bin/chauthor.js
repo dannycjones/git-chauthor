6f46d0be4183b05a907dc84c0d94bed65c4403aa
Daniel Carl Jones <danny@danielcarl.info>
2017-08-26 22:25:08 +0100

    Refactor simple-git usages to use promises

--- bin/chauthor.js
+++ bin/chauthor.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 const
-    git = require('simple-git')(),
+    git = require('simple-git/promise')(),
     inquirer = require('inquirer'),
     _ = require('lodash'),
     config = require('../config.js');
@@ -12,73 +12,13 @@ const
 
 function main() {
     if (authors) {
-        inquirer.prompt([
-            {
-                name: 'author',
-                message: 'Which author details would you like to use with this repository?',
-                type: 'list',
-                choices: authorChoices.concat([
-                    new inquirer.Separator(),
-                    'Manage authors...'
-                ])
-            },
-            {
-                name: 'action',
-                message: 'What would you like to do?',
-                type: 'list',
-                choices: [
-                    'Add author',
-                    'Remove authors'
-                ],
-                when: answers => answers.author === 'Manage authors...'
-            },
-            {
-                name: 'name',
-                message: 'Author name:',
-                when: answers => answers.action === 'Add author'
-            },
-            {
-                name: 'email',
-                message: 'Author email:',
-                when: answers => answers.action === 'Add author'
-            },
-            {
-                name: 'alias',
-                message: 'Author alias:',
-                when: answers => answers.action === 'Add author'
-            }
-        ]).then(answers => {
-            if (answers.action) {
-                switch (answers.action) {
-                    case 'Add author':
-                        authors.push({
-                            alias: answers.alias,
-                            email: answers.email,
-                            name: answers.name
-                        });
-                        config.save(authors);
-                        break;
-                    case 'Remove authors':
-                        inquirer.prompt([{
-                            name: 'authors',
-                            message: 'Which authors should be removed?',
-                            type: 'checkbox',
-                            choices: authorChoices
-                        }]).then(answers => {
-                            _.pullAll(authors, answers.authors);
-                            config.save(authors);
-                        }).catch(console.error);
-                        break;
-                    default:
-                        console.log('TODO: Take action when user wants to manage authors...');
-                        break;
-                }
+        checkInRepo().then(inRepo => {
+            if (inRepo) {
+                manageRepoAuthor();
             } else {
-                git.addConfig('user.name', answers.author.name);
-                git.addConfig('user.email', answers.author.email);
-                console.log('Git author config set.');
+                console.log('You are not in a git repository.');
             }
-        }).catch(console.error);
+        });
     } else {
         console.log('No authors found!');
         inquirer.prompt([
@@ -114,4 +54,85 @@ function createChoices(authors) {
     return choices;
 }
 
+function checkInRepo() {
+    return git.silent(true).raw(['rev-parse', '--is-inside-work-tree']);
+}
+
 main();
+
+function manageAuthors() {
+    inquirer.prompt([
+        {
+            name: 'action',
+            message: 'What would you like to do?',
+            type: 'list',
+            choices: [
+                'Add author',
+                'Remove authors'
+            ]
+        },
+        {
+            name: 'name',
+            message: 'Author name:',
+            when: answers => answers.action === 'Add author'
+        },
+        {
+            name: 'email',
+            message: 'Author email:',
+            when: answers => answers.action === 'Add author'
+        },
+        {
+            name: 'alias',
+            message: 'Author alias:',
+            when: answers => answers.action === 'Add author'
+        }
+    ]).then(answers => {
+        switch (answers.action) {
+            case 'Add author':
+                authors.push({
+                    alias: answers.alias,
+                    email: answers.email,
+                    name: answers.name
+                });
+                config.save(authors);
+                break;
+            case 'Remove authors':
+                inquirer.prompt([{
+                    name: 'authors',
+                    message: 'Which authors should be removed?',
+                    type: 'checkbox',
+                    choices: authorChoices
+                }]).then(answers => {
+                    _.pullAll(authors, answers.authors);
+                    config.save(authors);
+                }).catch(console.error);
+                break;
+            default:
+                console.log('TODO: Take action when user wants to manage authors...');
+                break;
+        }
+    }).catch(console.error);
+}
+
+function manageRepoAuthor() {
+    inquirer.prompt([
+        {
+            name: 'author',
+            message: 'Which author details would you like to use with this repository?',
+            type: 'list',
+            choices: authorChoices.concat([
+                new inquirer.Separator(),
+                'Manage authors...'
+            ])
+        }
+    ]).then(answers => {
+        if (answers.author !== 'Manage authors...') {
+            Promise.all([
+                git.addConfig('user.name', answers.author.name),
+                git.addConfig('user.email', answers.author.email)
+            ]).then(() => console.log('Git author config set.'), console.error);
+        } else {
+            manageAuthors();
+        }
+    }).catch(console.error);
+}
